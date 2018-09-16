214e76f0b27639b706d5e0bfb04908e0f34e58f1
Tomas Hanley <hanleyt@tcd.com>
2018-09-16 21:53:42 +0100

    remove code to implement

--- consumer-service/src/main/java/com/toasttab/consumer/service/ConsumerService.kt
+++ consumer-service/src/main/java/com/toasttab/consumer/service/ConsumerService.kt
@@ -1,24 +1,34 @@
 package com.toasttab.consumer.service
 
 import com.toasttab.food.service.AvailableFood
+import com.toasttab.food.service.DummyFoodService
+import com.toasttab.food.service.FoodService
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.StatusPages
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
+import io.ktor.locations.Location
+import io.ktor.locations.Locations
+import io.ktor.locations.get
+import io.ktor.locations.locations
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.html.a
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.li
+import kotlinx.html.p
+import kotlinx.html.ul
+import java.util.NoSuchElementException
 
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
 
-interface ConsumerAPI {
-
-    fun getAvailableFood(): List<AvailableFood>
-
-    fun orderFood(orderRequest: OrderRequest)
-
-}
-
-class ConsumerAPIImpl: ConsumerAPI {
-
-    override fun getAvailableFood(): List<AvailableFood> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
-    override fun orderFood(orderRequest: OrderRequest) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    server.start(wait = true)
 }
--- food-service/src/main/kotlin/com/toasttab/food/service/FoodService.kt
+++ food-service/src/main/kotlin/com/toasttab/food/service/FoodService.kt
@@ -1,31 +1,9 @@
 package com.toasttab.food.service
 
-import kotlinx.coroutines.experimental.delay
-
 interface FoodService {
     suspend fun getFoodNames(): List<String>
     suspend fun getFoodDetails(name:String): AvailableFood?
 }
 
 object DummyFoodService : FoodService {
-    override suspend fun getFoodNames(): List<String> {
-        delay(5000)
-        return listOf(
-                "Food1",
-                "Food2",
-                "Food3",
-                "Food4"
-        )
-    }
-
-    override suspend fun getFoodDetails(name: String): AvailableFood? {
-        delay(5000)
-        return when (name) {
-            "Food1" -> AvailableFood("Food1", "Summary1")
-            "Food2" -> AvailableFood("Food2", "Summary2")
-            "Food3" -> AvailableFood("Food3", "Summary3")
-            "Food4" -> AvailableFood("Food4", "Summary4")
-            else -> null
-        }
-    }
 }
